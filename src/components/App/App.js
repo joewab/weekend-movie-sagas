d0069d7d256d13f8d22d0ad32b41fb4ad604fb61
Joe AB <joe.anthonybrown@gmail.com>
2022-05-14 23:34:25 -0500

    styled with material ui, more styling on the way

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,19 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
 import Details from '../Details/Details';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      <h1>These are movies 🤷‍♂️</h1>
       <Router>       
-        <nav>
+        {/* <nav>
         <Link to="/">Home</Link>
         <Link to="/details">Details</Link>
 
-        </nav> 
+        </nav>  */}
         <Route path="/" exact>
           <MovieList />
         </Route>
--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';``
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import { CardContent } from '@material-ui/core';
 
 
-function MovieItem({movie}) {
+function MovieItem({ movie }) {
 
     const dispatch = useDispatch();
     const history = useHistory();
@@ -30,31 +26,19 @@ function MovieItem({movie}) {
         history.push('/details')
     }
     return (
-        <ImageListItem key={movie.id}  >
-              <img id="movie-poster"
-                src={`${movie.poster}?w=248&fit=crop&auto=format`}
-                srcSet={`${movie.poster}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                alt={movie.title}
-                loading="lazy"
-              />
-              <ImageListItemBar
-                title={movie.title}
-                // subtitle={movie.title}
-                actionIcon={
-                  <IconButton
-                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                    aria-label={`info about ${movie.title}`}
-                    onClick={fetchMovie}
-                  >
-                    <InfoIcon />
-                  </IconButton>
+        <Card elevation={20}>
+            <CardHeader
+                action={
+                    <IconButton>
+                        <InfoIcon onClick={fetchMovie} />
+                    </IconButton>
                 }
-              />
-            </ImageListItem>
-        // <div>
-        //     <h3>{movie.title}</h3>
-        //     <img src={movie.poster} alt={movie.title} onClick={fetchMovie} />
-        // </div>
+                title={movie.title}
+            />
+            <CardContent>
+                <img src={movie.poster} />
+            </CardContent>
+        </Card>
     )
 }
 
